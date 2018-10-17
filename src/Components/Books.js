b0e88e314dcbc69ca725719d312e4a274db5f622
matssh <mats.hagen2@gmail.com>
2018-10-17 20:57:04 +0200

    State oppdateres og fremsiden viser alle bøker i databasen som et eget kort

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,37 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import fire from '../utils/api';
 
-
-
-function BookGrid(props) {
-
-
-    return (
-
-        <div className="container">
-            <div className="row">
-
-                <div className="col">
-                    <div className="card" style={{ width: "18rem" }}>
-                        <img src={props.img} alt="book" className="card-img-top" />
-                        <div className="card-body">
-                            <h5 className="card-title">{props.title}</h5>
-                            <p className="card-text">{props.description}</p>
-                            <p className="card-text">{props.price}</p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    )
-}
-
-BookGrid.propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
-};
 
 
 class Books extends React.Component {
@@ -39,22 +8,29 @@ class Books extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: '',
-            description: '',
-            price: '',
+            books: []
         };
 
     }
 
     componentDidMount() {
 
-        let booksRef = fire.database().ref('Books/1');
+        let booksRef = fire.database().ref('Books');
         booksRef.on('value', snap => {
+            let books = snap.val();
+            let newState = [];
+            for (let book in books) {
+                newState.push({
+                    title: books[book]['Title'],
+                    description: books[book]['Description'],
+                    price: `${books[book]['Price']},-`
+                });
+            }
+
             this.setState({
-                title: snap.val()['Title'],
-                description: snap.val()['Description'],
-                price: `${snap.val()['Price']},-`
+                books: newState
             });
+
         });
     }
 
@@ -64,11 +40,26 @@ class Books extends React.Component {
 
         return (
 
-            <BookGrid
-                title={this.state.title}
-                description={this.state.description}
-                price={this.state.price}
-            />
+            <div className="container">
+                <div className="row">
+
+                    <div className="col">
+                        {this.state.books.map((book) => {
+                            return (
+                                <div className="card" key={book.title}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{book.title}</h5>
+                                        <p className="card-text">{book.description}</p>
+                                        <p className="card-text">{book.price}</p>
+                                    </div>
+                                </div>
+                            );
+                        })
+                        }
+                    </div>
+                </div>
+
+            </div>
 
         )
 
